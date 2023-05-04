9135f98040e8173d4210da175244da4795f7205f
이혁기 <gurrl9823@gmail.com>
2023-05-04 15:18:37 +0900

    패키지명 소문자로 변경

--- src/main/java/com/foodlink/foodlinkspringboot/controller/FoodLinkController.java
+++ src/main/java/com/foodlink/foodlinkspringboot/controller/FoodLinkController.java
@@ -1,4 +1,4 @@
-package com.foodlink.foodlinkspringboot.Controller;
+package com.foodlink.foodlinkspringboot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
