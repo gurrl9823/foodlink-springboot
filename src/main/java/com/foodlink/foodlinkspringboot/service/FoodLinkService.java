f8985b4d2791c50de6436b8040dfbcefd7490b81
이혁기 <gurrl9823@gmail.com>
2023-05-22 10:34:20 +0900

    flask 서버에서 받는 데이터를 List로 구성

--- src/main/java/com/foodlink/foodlinkspringboot/service/FoodLinkService.java
+++ src/main/java/com/foodlink/foodlinkspringboot/service/FoodLinkService.java
@@ -3,6 +3,7 @@ package com.foodlink.foodlinkspringboot.service;
 import com.foodlink.foodlinkspringboot.dto.ParamDto;
 import com.foodlink.foodlinkspringboot.dto.ResponseDto;
 import com.foodlink.foodlinkspringboot.repository.FoodLinkRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,11 +15,11 @@ public class FoodLinkService {
     @Autowired
     private FoodLinkRepository foodLinkRepository;
 
-    public ResponseDto getMenu(ParamDto requestDto) {
+    public List<ResponseDto> getMenu(ParamDto requestDto) {
 
         // param으로 들어온 재료가 저장소에 있는 재료명에 있는지 체크
 
-        ResponseDto recipe = foodLinkRepository.getMenu(requestDto.getIngredients());
+        List<ResponseDto> recipe = foodLinkRepository.getMenu(requestDto.getIngredients());
 
         return recipe;
     }
