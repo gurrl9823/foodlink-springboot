8f64c09c7fb4c9b0dc5ecff71897dd28f0d636ec
이혁기 <gurrl9823@gmail.com>
2023-05-23 23:27:54 +0900

    ResponseDto 변수 array에서 list로 변경

--- src/main/java/com/foodlink/foodlinkspringboot/dto/ResponseDto.java
+++ src/main/java/com/foodlink/foodlinkspringboot/dto/ResponseDto.java
@@ -12,6 +12,6 @@ import java.util.List;
 public class ResponseDto {
 
     private String foodName;
-    private String[] ingredients;
-    private String[] recipe;
+    private List<String> ingredients;
+    private List<String> recipe;
 }
